6ba431b47e409bbc0b80e98672283afd57363b08
kinjal <2kshah_34@gmail.com>
2025-12-19 15:26:56 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
 
   return {
-    base: "/insta-bill/", // 🔥 REQUIRED FOR GITHUB PAGES
-
-    server: {
-      port: 3000,
-      host: "0.0.0.0",
-    },
+    base: "/insta-bill/", // ✅ MUST match repo name
 
     plugins: [react()],
 
